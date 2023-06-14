fd2a1329f36e6e59611518ee614429724b9cd82b
RPE8 <vitali.shuman96@gmail.com>
2023-06-14 08:46:09 +0300

    Configurate tailwind and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCaseOnly",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@components/ui": path.resolve(__dirname, "./src/components/ui"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
     },
   },
 });
