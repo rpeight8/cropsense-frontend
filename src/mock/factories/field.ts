1b975db48e8066434d33203a4b4b829dce67229f
RPE8 <vitali.shuman96@gmail.com>
2023-06-18 15:08:19 +0300

    Add coords prop to mock

--- src/mock/factories/field.ts
+++ src/mock/factories/field.ts
@@ -1,7 +1,13 @@
 import { Factory } from "miragejs";
 import { faker } from "@faker-js/faker";
 
-import { Field } from "../../types";
+import { generatePolygon } from "../utils/generators";
+
+import { Field, FieldPolygon } from "../../types";
+
+
+
+
 
 export const fieldFactory = Factory.extend<Field>({
   id() {
@@ -9,5 +15,19 @@ export const fieldFactory = Factory.extend<Field>({
   },
   text() {
     return `${faker.company.buzzAdjective()} Field`;
+  },
+  coords() {
+    const multiplier = 0.2;
+    const lat = 52.434;
+    const minLat = lat - multiplier;
+    const maxLat = lat + multiplier;
+
+    const lng = 30.9754;
+    const minLng = lng - multiplier;
+    const maxLng = lng + multiplier;
+    return {
+      polygons: [generatePolygon(minLat, maxLat, minLng, maxLng)],
+      holes: [],
+    };
   },
 });
