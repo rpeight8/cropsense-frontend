e00993b6cb9e9e76a9217e832273c16b81a67811
RPE8 <vitali.shuman96@gmail.com>
2023-07-13 18:21:55 +0300

    Add initial logic for crop plant and harvest periods

--- src/features/crops/schemas.ts
+++ src/features/crops/schemas.ts
@@ -5,5 +5,13 @@ export const CropSchema = z.object({
   name: z.string(),
   color: z.string(),
 });
-
 export const CropsSchema = z.array(CropSchema);
+
+export const CropRotationSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  crops: z.array(CropSchema),
+  startDate: z.date(),
+  endDate: z.date(),
+});
+export const CropRotationsSchema = z.array(CropRotationSchema);
--- src/features/crops/types.ts
+++ src/features/crops/types.ts
@@ -3,3 +3,5 @@ import * as CropSchemas from "./schemas";
 
 export type Crop = z.infer<typeof CropSchemas.CropSchema>;
 export type Crops = z.infer<typeof CropSchemas.CropsSchema>;
+
+
